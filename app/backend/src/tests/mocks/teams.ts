89c4fc04ec9f396da3be44f198e1d71ecc509e94
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-01 18:45:27 -0300

    feat: create mock to test the "/teams" route

---
+++ app/backend/src/tests/mocks/teams.ts
@@ -0,0 +1,68 @@
+const mockTeams = [
+        {
+          "id": 1,
+          "teamName": 'Avaí/Kindermann',
+        },
+        {
+          "id": 1,
+          "teamName": 'Bahia',
+        },
+        {
+          "id": 1,
+          "teamName": 'Botafogo',
+        },
+        {
+          "id": 1,
+          "teamName": 'Corinthians',
+        },
+        {
+          "id": 1,
+          "teamName": 'Cruzeiro',
+        },
+        {
+          "id": 1,
+          "teamName": 'Ferroviária',
+        },
+        {
+          "id": 1,
+          "teamName": 'Flamengo',
+        },
+        {
+          "id": 1,
+          "teamName": 'Grêmio',
+        },
+        {
+          "id": 1,
+          "teamName": 'Internacional',
+        },
+        {
+          "id": 1,
+          "teamName": 'Minas Brasília',
+        },
+        {
+          "id": 1,
+          "teamName": 'Napoli-SC',
+        },
+        {
+          "id": 1,
+          "teamName": 'Palmeiras',
+        },
+        {
+          "id": 1,
+          "teamName": 'Real Brasília',
+        },
+        {
+          "id": 1,
+          "teamName": 'Santos',
+        },
+        {
+          "id": 1,
+          "teamName": 'São José-SP',
+        },
+        {
+          "id": 1,
+          "teamName": 'São Paulo',
+        },
+      ]
+
+export { mockTeams } ;
