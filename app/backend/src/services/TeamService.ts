4639ab6753b4dd4fa3781130f606eb5e9742806a
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-03 11:41:03 -0300

    feat: implements method "findByPK" in class "TeamService "

--- app/backend/src/services/TeamService.ts
+++ app/backend/src/services/TeamService.ts
@@ -1,4 +1,5 @@
 import { ModelStatic } from 'sequelize';
+import Errors from '../utils/errors/ErrorsMap';
 
 import TeamModel from '../database/models/TeamModel';
 import { Iteams } from '../interface/Iteams';
@@ -12,6 +13,16 @@ class TeamService implements Iteams {
     const teamModel = await this._teamModel.findAll();
 
     return teamModel;
+  }
+
+  async findByPk(id: number) {
+    const teamModel = await this._teamModel.findByPk(id);
+
+    if (!teamModel) {
+      throw new Errors('404', 'one or more "TeamId" not found');
+    }
+
+    return { type: '', message: teamModel };
   }
 }
 
