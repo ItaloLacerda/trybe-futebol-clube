3eb70ac1c1f44623df27fabb6908ebb8315379ce
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-03 18:37:00 -0300

    feat: fix syntax of private attributes in class

--- app/backend/src/controllers/TeamController.ts
+++ app/backend/src/controllers/TeamController.ts
@@ -5,17 +5,17 @@ import { IteamController } from '../interface/IteamController';
 
 class TeamsController implements IteamController {
   constructor(
-    private teamService: Iteams = new TeamService(),
+    private _teamService: Iteams = new TeamService(),
   ) {}
 
   async findAll(_req: Request, res: Response) {
-    return res.status(200).json(await this.teamService.findAll());
+    return res.status(200).json(await this._teamService.findAll());
   }
 
   async findByPk(req: Request, res: Response) {
     const { id } = req.params;
 
-    const teamService = await this.teamService.findByPk(Number(id));
+    const teamService = await this._teamService.findByPk(Number(id));
 
     return res.status(200).json(teamService.message);
   }
