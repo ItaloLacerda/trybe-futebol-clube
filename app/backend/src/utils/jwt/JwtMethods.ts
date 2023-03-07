541bd401f22af3849d08cd8e584be2f236bb46e0
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-07 00:46:55 -0300

    feat: create Jwt Methods class

---
+++ app/backend/src/utils/jwt/JwtMethods.ts
@@ -0,0 +1,16 @@
+import * as jwt from 'jsonwebtoken';
+
+export default class JwtMethods {
+  constructor(
+    private _secret: jwt.Secret = process.env.JWT_SECRET as jwt.Secret,
+  ) {}
+
+  createToken(payload: jwt.JwtPayload) {
+    const jwtConfig: jwt.SignOptions = {
+      expiresIn: '1h',
+      algorithm: 'HS256',
+    };
+
+    return jwt.sign(payload, this._secret, jwtConfig);
+  }
+}
