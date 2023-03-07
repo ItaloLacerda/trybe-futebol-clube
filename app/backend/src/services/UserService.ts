a101e20129b2af7bb4359f2f81f0f03e348de998
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-07 12:07:20 -0300

    feat: implements validations to the LoginUser method

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -1,5 +1,6 @@
 import { ModelStatic } from 'sequelize';
 import { compare } from 'bcryptjs';
+import UserValidation from '../utils/validations/UserValidation';
 import Errors from '../utils/errors/ErrorsMap';
 import IuserService from '../interface/IuserService';
 import UserModel from '../database/models/UserModel';
@@ -12,18 +13,26 @@ export default class UserService implements IuserService {
   ) {}
 
   async LoginUser(Email: string, Password: string) {
+    const { emailValidation, passwordValidation } = UserValidation;
+    const mensageError = 'Invalid email or password';
+
+    if (!emailValidation(Email) || !passwordValidation(Password)) {
+      throw new Errors('401', mensageError);
+    }
+
     const userModel = await this._teamModel.findOne({
       where: {
         email: Email,
       },
     });
 
-    if (!userModel) throw new Errors('401', 'Invalid email or password');
+    if (!userModel) throw new Errors('401', mensageError);
 
     const { password, id, email, role, username } = userModel;
+
     const validatePassword = await compare(Password, password);
 
-    if (!validatePassword) throw new Errors('401', 'Invalid email or password');
+    if (!validatePassword) throw new Errors('401', mensageError);
 
     return { token: this._jwt.createToken({ id, email, role, username }) };
   }
