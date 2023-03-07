61b901dd6f6eaa8dd247a2d75735fbea177364c6
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-07 00:51:27 -0300

    fix: fix TeamController import

--- app/backend/src/router/teamRouter.ts
+++ app/backend/src/router/teamRouter.ts
@@ -1,5 +1,5 @@
 import { Request, Router, Response } from 'express';
-import TeamController from '../controllers';
+import { TeamController } from '../controllers';
 
 const router = Router();
 const TeamsController = new TeamController();
