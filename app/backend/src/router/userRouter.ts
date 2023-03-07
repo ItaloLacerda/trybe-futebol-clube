7a185f6d3bcbe8710c58b22ac4a0b6e6328e2efd
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-07 00:57:37 -0300

    feat: create method "post" for route "/login"

---
+++ app/backend/src/router/userRouter.ts
@@ -0,0 +1,15 @@
+import { Request, Router, Response } from 'express';
+import UserValidation from '../middlewares/UserValidation';
+import { UserController } from '../controllers';
+
+const router = Router();
+const UsersController = new UserController();
+
+router.post(
+  '/',
+  UserValidation.ThereIsEmail,
+  UserValidation.ThereIsPassword,
+  (req: Request, res: Response) => UsersController.LoginUser(req, res),
+);
+
+export default router;
