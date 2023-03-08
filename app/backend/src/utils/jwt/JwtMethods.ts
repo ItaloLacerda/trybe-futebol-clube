dc5ca870573028461006ae28ae03c542e8eb386e
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-08 11:18:46 -0300

    feat: create method "tokenvalidation" in "JwtMethods"

--- app/backend/src/utils/jwt/JwtMethods.ts
+++ app/backend/src/utils/jwt/JwtMethods.ts
@@ -5,12 +5,14 @@ export default class JwtMethods {
     private _secret: jwt.Secret = process.env.JWT_SECRET as jwt.Secret,
   ) {}
 
-  createToken(payload: jwt.JwtPayload) {
+  createToken = (payload: jwt.JwtPayload) => {
     const jwtConfig: jwt.SignOptions = {
       expiresIn: '1h',
       algorithm: 'HS256',
     };
 
     return jwt.sign(payload, this._secret, jwtConfig);
-  }
+  };
+
+  tokenvalidation = (token: string) => jwt.verify(token, this._secret);
 }
