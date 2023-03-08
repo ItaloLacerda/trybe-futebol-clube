03ee99e58aa381c413c8da2bc96a9e296362ff1f
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-08 11:35:57 -0300

    feat: create get method for route "/login/role"

--- app/backend/src/router/userRouter.ts
+++ app/backend/src/router/userRouter.ts
@@ -1,15 +1,22 @@
-import { Request, Router, Response } from 'express';
+import { Router } from 'express';
 import UserValidation from '../middlewares/ThereIsUser';
+import Validations from '../middlewares/Validations';
 import { UserController } from '../controllers';
 
 const router = Router();
 const UsersController = new UserController();
+const ValidationsToken = new Validations();
+
+const { LoginUser, fetchRole } = UsersController;
+const { thereIsToken } = ValidationsToken;
 
 router.post(
   '/',
   UserValidation.ThereIsEmail,
   UserValidation.ThereIsPassword,
-  (req: Request, res: Response) => UsersController.LoginUser(req, res),
+  LoginUser,
 );
+
+router.get('/role', thereIsToken, fetchRole);
 
 export default router;
