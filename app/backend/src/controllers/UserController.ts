9ee67344ca2550389044859b58212e12650f95a9
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-08 11:23:40 -0300

    feat: implements fetchRole method in UserController

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -1,15 +1,28 @@
 import { Request, Response } from 'express';
+import { JwtPayload } from 'jsonwebtoken';
 import IuserService from '../interface/IuserService';
+import Jwt from '../utils/jwt/JwtMethods';
 import { UserService } from '../services';
 
 export default class UserController {
   constructor(
     private _userService: IuserService = new UserService(),
+    private _JwtMethods = new Jwt(),
   ) {}
 
-  async LoginUser(req: Request, res: Response) {
+  LoginUser = async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
     return res.status(200).json(await this._userService.LoginUser(email, password));
-  }
+  };
+
+  fetchRole = (req: Request, res: Response) => {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const user = this._JwtMethods.tokenvalidation(authorization) as JwtPayload;
+      const { role } = user;
+
+      return res.status(200).json({ role });
+    }
+  };
 }
