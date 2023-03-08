08b3fb0caa4ba5c332a9aeba0d219a28f396ccd7
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-08 12:04:46 -0300

    feat: create class MatchesModel

---
+++ app/backend/src/database/models/MatchesModel.ts
@@ -0,0 +1,47 @@
+import { BOOLEAN, Model, INTEGER } from 'sequelize';
+import db from '.';
+
+class MatchesModel extends Model {
+  declare id:number;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+MatchesModel.init({
+  id: {
+    primaryKey: true,
+    type: INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+export default MatchesModel;
