78c16d3cdb2350fb34ccd4f4a0101a61ef5332d8
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-09 14:27:22 -0300

    feat: implements updateScore in "/matches/id" route

--- app/backend/src/router/matchesRouter.ts
+++ app/backend/src/router/matchesRouter.ts
@@ -5,10 +5,12 @@ import Validations from '../middlewares/Validations';
 const router = Router();
 
 const { thereIsToken } = new Validations();
-const { fetchAllMatches, fetchMatchByProgress, finishAMatch } = new MatcheController();
+const { fetchAllMatches, fetchMatchByProgress, finishAMatch, updateScore } = new MatcheController();
 
 router.get('/', fetchMatchByProgress, fetchAllMatches);
 
 router.patch('/:id/finish', thereIsToken, finishAMatch);
+
+router.patch('/:id', thereIsToken, updateScore);
 
 export default router;
