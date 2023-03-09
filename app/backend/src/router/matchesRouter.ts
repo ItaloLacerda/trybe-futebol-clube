a119e5fc6ebf141caeef0094207f2a11320bc9db
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-09 19:47:04 -0300

    feat: implements post method on route "/matches"

--- app/backend/src/router/matchesRouter.ts
+++ app/backend/src/router/matchesRouter.ts
@@ -5,9 +5,16 @@ import Validations from '../middlewares/Validations';
 const router = Router();
 
 const { thereIsToken } = new Validations();
-const { fetchAllMatches, fetchMatchByProgress, finishAMatch, updateScore } = new MatcheController();
+const {
+  fetchAllMatches,
+  fetchMatchByProgress,
+  finishAMatch,
+  updateScore,
+  registerANewGame } = new MatcheController();
 
 router.get('/', fetchMatchByProgress, fetchAllMatches);
+
+router.post('/', thereIsToken, registerANewGame);
 
 router.patch('/:id/finish', thereIsToken, finishAMatch);
 
