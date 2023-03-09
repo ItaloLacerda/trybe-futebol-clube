5d6c3c2b5d15551d8485833d6d0330180df114de
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-09 14:04:07 -0300

    feat: create router "matches/:id/finish"

--- app/backend/src/router/matchesRouter.ts
+++ app/backend/src/router/matchesRouter.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { MatcheController } from '../controllers';
+import Validations from '../middlewares/Validations';
 
 const router = Router();
-const MatchesController = new MatcheController();
 
-const { fetchAllMatches, fetchMatchByProgress } = MatchesController;
+const { thereIsToken } = new Validations();
+const { fetchAllMatches, fetchMatchByProgress, finishAMatch } = new MatcheController();
 
 router.get('/', fetchMatchByProgress, fetchAllMatches);
+
+router.patch('/:id/finish', thereIsToken, finishAMatch);
 
 export default router;
