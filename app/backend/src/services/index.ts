0dc5bf6717eb1d9da9c473830fce73c634d2af20
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-10 18:37:54 -0300

    feat: implements LeaderboardService in barrel

--- app/backend/src/services/index.ts
+++ app/backend/src/services/index.ts
@@ -1,5 +1,6 @@
 import TeamService from './TeamService';
 import UserService from './UserService';
 import MatcheService from './MatcheService';
+import LeaderboardService from './LeaderboardService';
 
-export { TeamService, UserService, MatcheService };
+export { TeamService, UserService, MatcheService, LeaderboardService };
