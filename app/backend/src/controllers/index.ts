5220ce3c3440f2a54809ad8e1cbb5ef9f8b76540
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-10 18:39:03 -0300

    feat: implements LeaderboardController in barrel

--- app/backend/src/controllers/index.ts
+++ app/backend/src/controllers/index.ts
@@ -1,5 +1,6 @@
 import TeamController from './TeamController';
 import UserController from './UserController';
 import MatcheController from './MatcheController';
+import LeaderboardController from './LeaderboardController';
 
-export { TeamController, UserController, MatcheController };
+export { TeamController, UserController, MatcheController, LeaderboardController };
