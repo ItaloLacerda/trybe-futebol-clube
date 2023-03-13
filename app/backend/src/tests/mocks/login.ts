50cee67198a24538a64858bddc2ff36a75168c26
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-13 11:47:26 -0300

    test: create mock for login tests

---
+++ app/backend/src/tests/mocks/login.ts
@@ -0,0 +1,14 @@
+export const userLogin = {
+    id: 2,
+    username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO'
+}
+
+export const validLogin = {
+    email: 'user@user.com',
+    password: 'secret_user'
+}
+
+export const tokenMock = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJ1c2VyQHVzZXIuY29tIiwicm9sZSI6InVzZXIiLCJ1c2VybmFtZSI6IlVzZXIiLCJpYXQiOjE2Nzg3MTc2OTksImV4cCI6MTY3ODcyMTI5OX0.4FYjHLzh1JhAswc-jnbnOefFGnPTq0dPNxmxDCd8MQY"
