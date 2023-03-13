d1014b2ef1c754a7f520fcf82cfc3566ecc5e8d4
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-03-13 10:20:50 -0300

    feat: create interface ItotalGames

---
+++ app/backend/src/interface/IteamsInformation.ts
@@ -0,0 +1,14 @@
+export interface ItotalGames {
+  totalGames: number
+}
+
+export interface IteamsInformation {
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: number;
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories?: number
+}
