8222ac5b8c4a18d27e932098516ea4662e355dfe
Italo Lacerda <italo.lacerda2017@hotmail.com>
2023-04-18 17:45:15 -0300

    feat: creater new methods

--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -19,8 +19,39 @@ export default class LeaderboardService {
     );
 
     const ordeByTeams = (await informations).sort(this._Compare.totalPoints);
-    console.log(ordeByTeams);
     return ordeByTeams;
+  }
+
+  async teamsPerformanceInformationHome() {
+    const teams = await this._TeamModel.findAll();
+    const informations = Promise.all(
+      teams.map(async (team) => this
+        .homeTeamInfo(team.id, team.teamName)),
+    );
+
+    const ordeByTeams = (await informations).sort(this._Compare.totalPoints);
+    return ordeByTeams;
+  }
+
+  async homeTeamInfo(id: number, name: string) {
+    const { totalGames } = await this.getTotalHomeGames(id);
+    const totalVictories = await this.fectchTotalWinsAtHome(id);
+    const totalLosses = await this.fectchTotalLossesAtHome(id);
+    const totalDraws = await this.fectchTotalDrawsAtHome(id);
+    const goalsFavor = Number(Object.values(await this.fetchTotalHomeTeamGoals(id))[0]);
+    const goalsOwn = Number(Object.values(await this.lookForGoalsConcededAtHome(id))[0]);
+    const victoriesDraws = Object.values({ ...totalVictories, ...totalDraws });
+    const totalPoints = (Number(victoriesDraws[0]) * 3) + Number(victoriesDraws[1]);
+    return { name,
+      totalGames,
+      totalPoints,
+      ...totalVictories,
+      ...totalLosses,
+      ...totalDraws,
+      goalsFavor,
+      goalsOwn,
+      goalsBalance: goalsFavor - goalsOwn,
+      efficiency: Number(((totalPoints / (totalGames * 3)) * 100).toFixed(2)) };
   }
 
   async teamInformation(id: number, name: string) {
@@ -55,6 +86,17 @@ export default class LeaderboardService {
     return results;
   }
 
+  async getTotalHomeGames(id: number): Promise<ItotalGames> {
+    const [results] = await this._sequelize
+      .query(
+        `SELECT COUNT(*) AS totalGames FROM matches
+         WHERE in_progress = false 
+         AND (home_team_id = ${id});`,
+        { type: QueryTypes.SELECT },
+      ) as ItotalGames[];
+    return results;
+  }
+
   async fectchTotalVictories(id: number) {
     const [results] = await this._sequelize
       .query(
@@ -62,6 +104,17 @@ export default class LeaderboardService {
           WHERE in_progress = false 
           AND ((home_team_id = ${id} AND home_team_goals > away_team_goals) 
             OR (away_team_id = ${id} AND away_team_goals > home_team_goals));`,
+        { type: QueryTypes.SELECT },
+      );
+    return results;
+  }
+
+  async fectchTotalWinsAtHome(id: number) {
+    const [results] = await this._sequelize
+      .query(
+        ` SELECT COUNT(*) as totalVictories FROM matches
+          WHERE in_progress = false 
+          AND (home_team_id = ${id} AND home_team_goals > away_team_goals);`,
         { type: QueryTypes.SELECT },
       );
     return results;
@@ -79,12 +132,35 @@ export default class LeaderboardService {
     return results;
   }
 
+  async fectchTotalLossesAtHome(id: number) {
+    const [results] = await this._sequelize
+      .query(
+        ` SELECT COUNT(*) as totalLosses FROM matches
+          WHERE in_progress = false 
+          AND (home_team_id = ${id} AND home_team_goals < away_team_goals)`,
+        { type: QueryTypes.SELECT },
+      );
+    return results;
+  }
+
   async fectchTotalDraws(id: number) {
     const [results] = await this._sequelize
       .query(
         ` SELECT COUNT(*) as totalDraws FROM matches 
           WHERE in_progress = false 
           AND (home_team_id = ${id} OR away_team_id = ${id}) 
+          AND home_team_goals = away_team_goals;`,
+        { type: QueryTypes.SELECT },
+      );
+    return results;
+  }
+
+  async fectchTotalDrawsAtHome(id: number) {
+    const [results] = await this._sequelize
+      .query(
+        ` SELECT COUNT(*) as totalDraws FROM matches 
+          WHERE in_progress = false 
+          AND (home_team_id = ${id}) 
           AND home_team_goals = away_team_goals;`,
         { type: QueryTypes.SELECT },
       );
